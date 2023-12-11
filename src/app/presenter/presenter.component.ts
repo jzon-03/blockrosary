2eecee8237630bbc0428a82ca88a38c9c72a4c43
jguevarra03 <100967014+jguevarra03@users.noreply.github.com>
2023-12-11 10:45:25 -0500

    update

--- src/app/presenter/presenter.component.ts
+++ src/app/presenter/presenter.component.ts
@@ -1,16 +1,17 @@
-import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { PrayerService } from '../services/prayer.service';
 import {UntypedFormBuilder, UntypedFormControl, UntypedFormGroup} from '@angular/forms';
 import { DOCUMENT } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { SettingsComponent } from './settings/settings.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-presenter',
   templateUrl: './presenter.component.html',
   styleUrls: ['./presenter.component.css']
 })
-export class PresenterComponent implements OnInit {
+export class PresenterComponent implements AfterViewInit {
 
   // @ViewChild('label', {static:true}) public mys!:ElementRef<HTMLDivElement>
   mainPrayer:any
@@ -20,11 +21,15 @@ export class PresenterComponent implements OnInit {
   name: any;
   timeoutHandler: any;
 
+
   constructor(
-  private _dialog:MatDialog
+  private _dialog:MatDialog,
+  private _activatedRoute: ActivatedRoute,
+  private _renderer: Renderer2
     ) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+
   }
 
   scrollToElement($element:any): void {
